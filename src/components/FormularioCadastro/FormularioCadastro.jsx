19aa6a343fb84ab883d76ab73f86c544fd13001b
Paulo Andrino <pauloandrino@yahoo.com.br>
2021-05-31 12:14:51 -0300

    Bind "this".

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -20,7 +20,7 @@ class FormularioCadastro extends Component {
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
-                    onChange={this.handleMudancaTitulo}
+                    onChange={this.handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                     rows={15}
